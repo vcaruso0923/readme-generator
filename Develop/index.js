fcc01ad0245fc0f4f04673b02bf99e1f11de7f44
vcaruso <vinnycar0923@gmail.com>
2020-07-16 15:00:45 -0700

    added necessary questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,92 @@
+const inquirer = require('inquirer');
+
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the project title!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your Github username?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your Github username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Explain the installation process:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use:',
+    },
+    {
+        type: 'checkbox',
+        name: 'licence',
+        message: 'Select the license type for your project:',
+        choices: [
+            'None',
+            'Apache License 2.0',
+            'GNU General Public License v3.0',
+            'MIT License',
+            'BSD 2-Clause "Simplified" License',
+            'BSD 3-Clause "New" or "Revised" License',
+            'Boost Software License 1.0',
+            'Creative Commons Zero v1.0 Universal',
+            'Eclipse Public License 2.0',
+            'GNU Affero General Public License v3.0',
+            'GNU General Public License v2.0',
+            'GNU Lesser General Public License v2.1',
+            'Mozilla Public License 2.0',
+            'The Unlicense'
+        ]
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide guidelinies to contributing to your project:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Then provide examples on how to run tests for your project:',
+    },
 ];
 
 // function to write README file
